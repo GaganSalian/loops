5ba8fadb2859e3c646aa4531409258b5bbfe2550
GaganSalian <gagansalian04102005@gmail.com>
2024-09-10 09:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,18 @@ var isPrime = true;
 
 if (isNaN(number)) {
     document.write("Please enter a valid number.");
-} else if (number == 1) {
-    document.write("1 is neither a prime nor a composite number");
-} else if (number > 1) {
-    for (var i = 2; i <=number/2 ; i++) {
+} else if (number <= 1) {
+    document.write(number + " is neither a prime nor a composite number.");
+} else {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
         if (number % i == 0) {
             isPrime = false;
             break;
         }
     }
-    if (isPrime == true) {
-        document.write(number + " is a prime number");
+    if (isPrime) {
+        document.write(number + " is a prime number.");
     } else {
-        document.write(number + " is not a prime number");
+        document.write(number + " is not a prime number.");
     }
-} else {
-    document.write("Number is not a prime number.");
 }
